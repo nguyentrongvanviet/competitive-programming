40662364c34860d672cfe4f93890c82c47644ea0
nguyentrongvanviet <nguyentrongvanviet@gmail.com>
2023-11-17 15:36:24 +0700

    comment

--- VIet_love_KA.bat
+++
@@ -1,8 +0,0 @@
-echo off
-git init
-git remote add origin https://github.com/nguyentrongvanviet/competitive-programming.git
-git add .
-set /p comment=What do you want to comment ? 
-git commit -m comment
-git push -f origin main
-echo on
