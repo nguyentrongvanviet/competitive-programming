5c62d1c145c03f2c65531a795a38baa33d331416
nguyentrongvanviet <nguyentrongvanviet@gmail.com>
2023-12-10 23:14:07 +0700

    pvh contest

--- 2023/9.12.2023/numtable.cpp
+++ 2023/9.12.2023/numtable.cpp
@@ -5,9 +5,10 @@
 *            Hometown :  Quang Ngai , Viet Nam .               *
 * Khanh An is my lover :) the more I code  , the nearer I am   *
 ****************************************************************/
-#define TASK "gcdpath"
+#define TASK "numtable"
 #define INPUT TASK".INP" 
 #define OUTPUT TASK".OUT"
+
 bool mtt = 0 ;
 int test = 1 ;  
 
@@ -57,160 +58,102 @@ int yy[] = {-1,0,1,0} ;
 
 const db PI = acos(-1) , EPS = 1e-9;
 const ll inf = 1e18 , cs = 331 , sm = 1e9+7; 
-const int N = 3e5+5 , oo = 2e9 , LO = 17 , CH = 26 ; 
+const int N = 1e6+5 , oo = 2e9 , LO = 17 , CH = 26 ; 
 
 
-int n ; 
-vi g[N] ;
-int a[N] ; 
-
-void doc()
-{	
-	cin>> n;    
-	FOR(i,1,n)cin>>a[i] ;  
-	FOR(i,1,n-1)
-	{
-		int u ,v; cin>> u >>v; 
-		g[u].pb(v) ; 
-		g[v].pb(u) ; 
+int n , m ;
+ve<vi>a ; 
+int nt[(int)1e7+5];
+int cnt = 0; 
+int ID[(int)1e7+5];
+int val[664579+5];
+void snt()
+{
+	FOR(i,2,1e7)if(nt[i]==0)
+	{	
+		nt[i] =i ; 
+		FOR(j,i,1e7/i)nt[i*j] = i; 
+		cnt++ ; 
+		val[cnt]=i;
+		ID[i] = cnt; 
 	}
 }
 
-// namespace sub1
-// {
-// 	ll res = 0 ;
-// 	void dfs(int u ,int p ,int cur)
-// 	{
-// 		cur = gcd(cur,a[u]) ; 
-// 		res+=cur; 
-// 		for(auto v :g[u])if(v!=p)
-// 		{		
-// 			dfs(v,u,cur) ;
-// 		}
-// 	}
-//     void xuly()
-//     {
-//     	FOR(i,1,n)
-//     	{
-//     		dfs(i,0,a[i]) ;
-//     		res-=a[i] ; 
-//     	}
-//     	cout<<res/2; 
-//     }
-// }
-// namespace sub2
-// {
-// 	int P[N][LO+1] ;  
-// 	int VAL[N][LO+1] ;
-// 	int h[N] ; 
-// 	ll res = 0 ;
-// 	void dfs(int u ,int p )
-// 	{
-// 		VAL[u][0] = a[u] ; 
-// 		for(auto v:g[u])if(v!=p)
-// 		{
-// 			h[v] = h[u]+1 ;
-// 			P[v][0] = u ; 
-// 			dfs(v,u) ;
-// 		}
-// 	}
-// 	int lca(int u ,int v)
-// 	{
-// 		if(h[u]<h[v])swap(u,v) ;
-// 		int ans = 0 ; 
-// 		FORD(i,LO,0)if(h[u]-M(i)>=h[v])
-// 		{
-// 			ans=gcd(ans,VAL[u][i]) ;
-// 			u=P[u][i] ; 
-// 		}
-// 		if(u==v)
-// 		{
-// 			ans=gcd(ans,a[u]) ; 
-// 			return ans; 
-// 		}
-// 		FORD(i,LO,0)
-// 		{
-// 			int nu = P[u][i] ; 
-// 			int nv = P[v][i] ; 
-// 			if(nu!=nv)
-// 			{
-// 				ans=gcd(ans,VAL[u][i]) ; 
-// 				ans=gcd(ans,VAL[v][i]) ; 
-// 				u=nu ;
-// 				v=nv ;
-// 			}
-// 		}
-// 		ans=gcd(ans,VAL[u][1]) ; 
-// 		ans=gcd(ans,VAL[v][0]) ;
-// 		return ans ;
-// 	}
-// 	void xuly()
-// 	{
-// 		dfs(1,0) ;
-// 		FOR(j,1,LO)FOR(i,1,n)P[i][j]=P[P[i][j-1]][j-1] , VAL[i][j] = gcd(VAL[i][j-1],VAL[P[i][j-1]][j-1]) ;
-// 		FOR(i,1,n)FOR(j,i+1,n)
-// 		{
-// 			res+=lca(i,j) ; 
-// 		}
-// 		cout<<res<<el; 
-// 	}
-// }
-namespace sub3
+struct pt
 {
-	int pa[N] , sz[N] , P[N] , h[N] ;
+	int x , y; 
+};
+ve<pt>pos[664579+5] ;
+void doc()
+{
+	snt() ;
+	cin>> n >> m; 
+	a.resize(n+2,vi(m+2,0)) ; 
+	FOR(i,1,n)FOR(j,1,m)
+	{
+		cin>>a[i][j] ;
+		int cur = a[i][j] ; 
+		while(cur>1)
+		{
+			int d  = nt[cur] ;
+			pos[ID[d]].pb({i,j}) ;  
+			while(cur%d==0)cur/=d;
+		}
+	}
+}
+
+namespace sub1
+{
+	int pa[N],sz[N]; 
+	int C(int x ,int y)
+	{
+		return (x-1)*m+y ; 
+	}
 	int goc(int u)
 	{
 		return pa[u] == u ? u : pa[u] = goc(pa[u]) ;
 	}
-	ll merge(int u ,int v)
+	void hop(int u ,int v)
 	{
 		int chau = goc(u) ;
 		int chav = goc(v) ;
-		if(chau==chav)return 0;
+		if(chau==chav)return ; 
 		pa[chau] = chav; 
 		sz[chav]+=sz[chau] ; 
-		return 1ll*(sz[chav]-sz[chau])*sz[chau] ;
 	}
-	const int VAL = 1500000  ;
-	vi node[VAL+5] ; 
-	void dfs(int u ,int p)
-	{
-		for(auto v:g[u])if(v!=p)
-		{
-			P[v] = u; 
-			h[v] = h[u]+1 ;
-			dfs(v,u) ; 
-		}
-	}
-	ll sl[VAL+5] ; 
-	void xuly()
-	{
-		FOR(i,1,n)node[a[i]].pb(i),pa[i] = i , sz[i] = 1 ; 
-		dfs(1,0) ; 
-		FORD(i,VAL,1) 
-		{
-			vi tmp ; 
-			for(auto u :node[i])tmp.pb(u) ; 
-			FOR(j,2,VAL/i)
-			{
-				sl[i]-=sl[i*j] ;
-				for(auto u :node[i*j])tmp.pb(u) ;  
-			} 
-			for(auto u :tmp)if(u!=1)
-			{
-				int p = P[u] ; 
-				if(a[p]%i==0)sl[i]+=merge(u,p) ; 
-			}
-			for(auto u :tmp)pa[u] = u , sz[u] = 1;  
-		}
-		ll res = 0; 	
-		FOR(i,1,VAL)
-		{
-			res+=sl[i]*i ;
-		}
-		cout<<res<<el ; 
-	}
+
+    void xuly()
+    {
+    	FOR(i,1,n*m)sz[i]=1,pa[i]=i;
+    	int res =0 ; 
+    	FOR(i,1,cnt)
+    	{
+    		for(auto u:pos[i])
+    		{
+    			int x=u.x; 
+    			int y=u.y;
+    			FORN(j,0,4)
+    			{
+    				int nx = x+xx[j] ;
+    				int ny = y+yy[j] ; 
+    				if(a[nx][ny]&&a[nx][ny]%val[i]==0)
+    				{
+    					hop(C(nx,ny),C(x,y)) ; 
+    				}
+    			}
+    		}
+    		for(auto u :pos[i])
+    		{
+    			int c = C(u.x,u.y) ;
+    			maxi(res,sz[C(u.x,u.y)]) ; 
+    			pa[c] = c; 
+    			sz[c] = 1; 
+    		}
+    	}
+    	cout<<res<<el ;
+    }
 }
+
 /*  DON'T BELIEVE LOVE WILL INSPIRE YOU ->  TRAIN HARDER ->  YOU WILL GET THE LOVE YOU WANT !!*/
 
 signed main()
@@ -227,13 +170,11 @@ signed main()
         freopen("text.OUT","w",stdout) ;   
     }
     if(mtt)cin>>  test;
-    int sub; cin>>sub  ; 
+    int sub ;cin>>sub ;
     FOR(i,1,test)
     {
         doc() ; 
-        // if(sub<=2)sub1::xuly() ;
-        // if(sub==2)sub2::xuly() ;  
-        sub3::xuly() ; 
+        sub1::xuly() ; 
     }
     cerr<<el<<"Love KA very much !!! " << clock() <<"ms"<<el;
 }
