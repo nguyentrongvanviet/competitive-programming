80d00e05b273f47ecc3d3acf4ec764bf7249ca59
nguyentrongvanviet <nguyentrongvanviet@gmail.com>
2025-10-03 22:34:29 +0700

    GrayCode.cpp

---
+++ 2024/CSES/GraphPathI.cpp
@@ -0,0 +1,141 @@
+/***************************************************************
+*             Author : Nguyen Trong Van Viet                   *
+*                        Age : 17                              *
+*      School : 12T2 Le Khiet High School for the Gifted       *
+*            Hometown :  Quang Ngai , Viet Nam .               *
+* Khanh An is my lover :) the more I code  , the nearer I am   *
+****************************************************************/
+#define TASK "text"
+#define INPUT TASK".INP" 
+#define OUTPUT TASK".OUT"
+
+bool mtt = 0 ;
+int test = 1 ;  
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+#define             ll  long long 
+#define             db  double 
+#define             ve  vector 
+#define             vi  vector<int>
+#define            vll  vector<ll>
+#define            str  string
+#define             pb  push_back
+#define             pk  pop_back
+#define             el  '\n'
+#define            pii  pair<int,int>
+#define            pll  pair<ll,ll>
+#define             mp  make_pair 
+#define             fi  first 
+#define             se  second
+#define         uni(a)  sort(all(a)),a.resize(unique(all(a))-a.begin()) 
+#define     FOR(i,a,b)  for(int i=(int)(a);i<=(int)(b);i++)
+#define    FORD(i,a,b)  for(int i=(int)(a);i>=(int)(b);i--)
+#define    FORN(i,a,b)  for(int i=(int)(a);i<(int)(b);i++)
+#define         all(a)  a.begin(),a.end()  
+#define           btpc  __builtin_popcountll
+#define             LB  lower_bound
+#define             UB  upper_bound 
+#define            tct  template<class T>
+#define     BIT(msk,i)  (msk>>(i)&1)
+
+ll lg(ll a){return __lg(a);}
+ll sq(ll a){return a*a;}  
+ll gcd(ll a,ll b){return __gcd(a,b);} 
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+ll rd(ll l , ll r ){return l+1LL*rand()*rand()*rand()%(r-l+1);}
+
+#define prt(a,n) FOR(i,1,n)cout<<a[i]<<" ";cout<<el;
+#define prv(a) for(auto v:a)cout<<v<<" "; cout<<el; 
+
+tct bool mini(T& a,T b){return (a>b)?a=b,1:0;}
+tct bool maxi(T& a,T b){return (a<b)?a=b,1:0;}
+
+int xx[] = {0,0,-1,0,1}; 
+int yy[] = {0,-1,0,1,0};
+
+const db PI = acos(-1) , EPS = 1e-9;
+const ll inf = 1e18 , cs = 331 , sm = 1e9+7; 
+const int N = 2e5+5 , oo = 2e9 , LO = 17 , CH = 26 ; 
+
+int n , m , k ; 
+
+struct MT
+{
+    vector<vector<ll>> mt ; 
+    int n , m ; 
+    MT(int n = 0 , int m = 0 ):n(n),m(m) 
+    {
+        mt.resize(n+1,vector<ll>(m+1,0)) ;
+    }
+    MT operator*(const MT &b)const
+    {
+        MT c(n,b.m) ; 
+        FOR(i,1,n)
+        {
+            FOR(j,1,b.m)
+            {
+                FOR(t,1,m)
+                {
+                    c.mt[i][j] = (c.mt[i][j] + mt[i][t]*b.mt[t][j])%sm ; 
+                }
+            }
+        }
+        return c ; 
+    }
+    MT operator^(ll k)const
+    {
+        MT res(n,n) , a = *this ; 
+        FOR(i,1,n) res.mt[i][i] = 1 ; 
+        while(k)
+        {
+            if(k&1) res = res*a ; 
+            a = a*a ; 
+            k>>=1 ; 
+        }
+        return res ;
+    }
+} ; 
+
+void doc()
+{
+    cin >> n >> m >> k ; 
+    MT graph(n,n) ; 
+    for(int i=1;i<=m;i++){
+        int u , v ; cin >> u >> v ; 
+        graph.mt[u][v]++ ;  
+    }
+    graph = graph^k ; 
+    MT start(1,n) ; 
+    start.mt[1][1] = 1;  
+    cout << (start*graph).mt[1][n] << el ;
+}
+
+namespace sub1
+{
+    void xuly()
+    {
+
+    }
+}
+
+/*  DON'T BELIEVE LOVE WILL INSPIRE YOU ->  TRAIN HARDER ->  YOU WILL GET THE LOVE YOU WANT !!*/
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);srand(time(0)); 
+    if(fopen(INPUT,"r"))
+    {
+        freopen(INPUT ,"r",stdin);
+        freopen(OUTPUT,"w",stdout);
+    }
+    if(mtt)cin>>test;
+
+    FOR(i,1,test)
+    {
+        doc() ; 
+        sub1::xuly() ; 
+    }
+    cerr<<el<<"Love KA : " << clock() <<"ms"<<el;
+}
---
+++ 2024/CSES/GrayCode.cpp
@@ -0,0 +1,139 @@
+/***************************************************************
+*             Author : Nguyen Trong Van Viet                   *
+*                        Age : 17                              *
+*      School : 12T2 Le Khiet High School for the Gifted       *
+*            Hometown :  Quang Ngai , Viet Nam .               *
+* Khanh An is my lover :) the more I code  , the nearer I am   *
+****************************************************************/
+#define TASK "text"
+#define INPUT TASK".INP" 
+#define OUTPUT TASK".OUT"
+
+bool mtt = 0 ;
+int test = 1 ;  
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+#define             ll  long long 
+#define             db  double 
+#define             ve  vector 
+#define             vi  vector<int>
+#define            vll  vector<ll>
+#define            str  string
+#define             pb  push_back
+#define             pk  pop_back
+#define             el  '\n'
+#define            pii  pair<int,int>
+#define            pll  pair<ll,ll>
+#define             mp  make_pair 
+#define             fi  first 
+#define             se  second
+#define         uni(a)  sort(all(a)),a.resize(unique(all(a))-a.begin()) 
+#define     FOR(i,a,b)  for(int i=(int)(a);i<=(int)(b);i++)
+#define    FORD(i,a,b)  for(int i=(int)(a);i>=(int)(b);i--)
+#define    FORN(i,a,b)  for(int i=(int)(a);i<(int)(b);i++)
+#define         all(a)  a.begin(),a.end()  
+#define           btpc  __builtin_popcountll
+#define             LB  lower_bound
+#define             UB  upper_bound 
+#define            tct  template<class T>
+#define     BIT(msk,i)  (msk>>(i)&1)
+
+ll lg(ll a){return __lg(a);}
+ll sq(ll a){return a*a;}  
+ll gcd(ll a,ll b){return __gcd(a,b);} 
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+ll rd(ll l , ll r ){return l+1LL*rand()*rand()*rand()%(r-l+1);}
+
+#define prt(a,n) FOR(i,1,n)cout<<a[i]<<" ";cout<<el;
+#define prv(a) for(auto v:a)cout<<v<<" "; cout<<el; 
+
+tct bool mini(T& a,T b){return (a>b)?a=b,1:0;}
+tct bool maxi(T& a,T b){return (a<b)?a=b,1:0;}
+
+int xx[] = {0,0,-1,0,1}; 
+int yy[] = {0,-1,0,1,0};
+
+const db PI = acos(-1) , EPS = 1e-9;
+const ll inf = 1e18 , cs = 331 , sm = 1e9+7; 
+const int N = 2e5+5 , oo = 2e9 , LO = 17 , CH = 26 ; 
+
+int n; 
+
+void doc()
+{
+    auto bitrepresent = [](int x, int n)
+    {
+        str res = "" ; 
+        FORD(i,n-1,0)
+        {
+            if(BIT(x,i)) res+='1' ; 
+            else res+='0' ;  
+        } 
+        return res ; 
+    } ;
+    cin>> n; 
+    vector<int>deg(1<<n,0) ;
+    vector<int>dd(1<<n,0) ; 
+    cout<<bitrepresent(0,n)<<el; 
+    for(int st = 0 , num = 1 ;num<(1<<n);num++)
+    {
+        dd[st] = 1 ; 
+        for(int i=0;i<n;i++)
+        {
+            deg[st^(1<<i)]++ ;
+        }  
+        int ne  = 0 ; 
+        for(int i=0;i<n;i++)
+        {
+            if(!dd[st^(1<<i)])
+            {
+                if(ne==0)
+                {
+                    ne = st^(1<<i) ; 
+                }
+                else
+                {
+                    if(deg[st^(1<<i)] >deg[ne])
+                    {
+                        ne = st^(1<<i) ; 
+                    }
+                }
+            }
+        }
+        // if(ne==0)break;
+        assert(ne!=0) ; 
+        st = ne ; 
+        // cout<<st<<endl;
+        cout<<bitrepresent(st,n)<<el;
+    }
+}
+
+namespace sub1
+{
+    void xuly()
+    {
+
+    }
+}
+
+/*  DON'T BELIEVE LOVE WILL INSPIRE YOU ->  TRAIN HARDER ->  YOU WILL GET THE LOVE YOU WANT !!*/
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);srand(time(0)); 
+    if(fopen(INPUT,"r"))
+    {
+        freopen(INPUT ,"r",stdin);
+        freopen(OUTPUT,"w",stdout);
+    }
+    if(mtt)cin>>test;
+
+    FOR(i,1,test)
+    {
+        doc() ; 
+        sub1::xuly() ; 
+    }
+    cerr<<el<<"Love KA : " << clock() <<"ms"<<el;
+}
